8b13e21aaee9a932313c20b855666aea33453b27
lanjewarakash <lanjewarakash72@gmail.com>
2022-11-27 23:58:42 +0530

    [Akash]: Added Check Two Strings Are Equal Or Not

---
+++ src/com/blz/IsStringEqual.java
@@ -0,0 +1,15 @@
+package com.blz;
+
+public class IsStringEqual {
+    public static void main(String[] args) {
+        String str = "Akash";
+        String str1 = "Akash";
+        boolean valueOne = str.equals(str1);
+        System.out.println(valueOne);
+
+        String name = "Akash";
+        String sirName = "Lanjewar";
+        boolean valueTwo = name.equals(sirName);
+        System.out.println(valueTwo);
+    }
+}
