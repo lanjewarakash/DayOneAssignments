9d44f4640790017f1b0cad5f10aa0fd386440660
lanjewarakash <lanjewarakash72@gmail.com>
2022-11-27 23:55:16 +0530

    [Akash]: Added Default Value Of All Primitive Data Types Of Java

---
+++ src/com/blz/PrimitiveDefaultValue.java
@@ -0,0 +1,25 @@
+package com.blz;
+
+public class PrimitiveDefaultValue
+{
+    static byte b;
+    static short s;
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+    static boolean b1;
+    static char c;
+
+    public static void main(String[] args) {
+        System.out.println("The Default Of Byte Is :" + b);
+        System.out.println("The Default Of Short Is :" + s);
+        System.out.println("The Default Of Int Is :" + i);
+        System.out.println("The Default Of Long Is :" + l);
+        System.out.println("The Default Of Float Is :" + f);
+        System.out.println("The Default Of Double Is :" + d);
+        System.out.println("The Default Of Boolean Is :" + b1);
+        System.out.println("The Default Of Char Is :" + c);
+
+    }
+}
